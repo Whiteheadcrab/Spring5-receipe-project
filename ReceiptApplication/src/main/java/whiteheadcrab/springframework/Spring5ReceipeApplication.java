e542716145b7d17ce909081b303cfbc5a935a8bd
Whiteheadcrab <lettersorstudy@gmail.com>
2020-03-13 19:55:01 +0100

    Version 0.1.1 Initialization Project and README file

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/Spring5ReceipeApplication.java
@@ -0,0 +1,13 @@
+package whiteheadcrab.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReceipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5ReceipeApplication.class, args);
+    }
+
+}
---
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/Spring5ReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package whiteheadcrab.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ReceipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
