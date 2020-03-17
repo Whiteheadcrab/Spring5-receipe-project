bfbaf6eae950fee218db1d327df17743e8da8b09
Whiteheadcrab <lettersorstudy@gmail.com>
2020-03-17 13:31:04 +0100

    Version 0.4 Small changes for checking possibility of Spring Dev Tools

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/IndexController.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/IndexController.java
@@ -9,6 +9,7 @@ public class IndexController
     @RequestMapping({"/","","/index"})
     public String getIndexPage()
     {
+        System.out.println("Some message to say 122323");
         return "index";
     }
 }
