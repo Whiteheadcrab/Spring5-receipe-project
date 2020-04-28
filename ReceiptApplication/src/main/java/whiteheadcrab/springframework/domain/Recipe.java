4de7daa230a438cc544161a11579e9857c1289bd
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-28 23:20:10 +0200

    Version 0.9 Implemented data with types for Recipt part

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Recipe.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Recipe.java
@@ -1,4 +1,15 @@
 package whiteheadcrab.springframework.domain;
 
 public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty - for future implementations
+    private Byte[] image;
 }
