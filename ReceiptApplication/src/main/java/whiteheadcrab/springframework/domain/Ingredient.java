1343d62c517a2f60389790890315f822ed8b81e4
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-30 19:47:48 +0200

    Version 0.11 Implemented Ingredient class with basic set of variables

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Ingredient.java
@@ -0,0 +1,11 @@
+package whiteheadcrab.springframework.domain;
+
+import java.math.BigDecimal;
+
+public class Ingredient
+{
+    private String description;
+    private BigDecimal amount;
+
+    //private UnitOfMeasure uom;
+}
