189927d038e8f1df0dbbb3e131363cb37fb5f9f8
Whiteheadcrab <lettersorstudy@gmail.com>
2020-05-03 16:56:30 +0200

    Version 0.14 Implemented difficulty criteria for Receipts .

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package whiteheadcrab.springframework.domain;
+
+public enum  Difficulty
+{
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Recipe.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/domain/Recipe.java
@@ -28,6 +28,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -109,5 +112,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
