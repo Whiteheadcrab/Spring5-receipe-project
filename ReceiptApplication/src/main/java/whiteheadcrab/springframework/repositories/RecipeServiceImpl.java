8d4e7044f0045b1407d9e9221b07ebd169a6d6fe
Whiteheadcrab <lettersorstudy@gmail.com>
2020-06-07 09:06:44 +0200

    Version 0.20.2 Practise of different using of Lombok . Category has been modified by plugin , when Recipe has been changed manually by using Lombok annotations

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/repositories/RecipeServiceImpl.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/repositories/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package whiteheadcrab.springframework.repositories;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import whiteheadcrab.springframework.domain.Recipe;
 import whiteheadcrab.springframework.services.RecipeService;
@@ -7,6 +8,7 @@ import whiteheadcrab.springframework.services.RecipeService;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService
 {
@@ -20,8 +22,8 @@ public class RecipeServiceImpl implements RecipeService
     @Override
     public Set<Recipe> getRecipes()
     {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepositories.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
