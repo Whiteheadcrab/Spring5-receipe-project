c0039d688d0a856f7e0ec6c09f5e34f33979d947
Whiteheadcrab <lettersorstudy@gmail.com>
2020-07-08 19:45:16 +0200

    Version 1.9.4 Implemented RecipeController with changes in RecipeService . Need to create Test for RecipeController

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
@@ -0,0 +1,26 @@
+package whiteheadcrab.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import whiteheadcrab.springframework.services.RecipeService;
+
+@Controller
+public class RecipeController
+{
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService)
+    {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model)
+    {
+        model.addAttribute("recipe",recipeService.findById(new Long(id)));
+
+        return "recipe/show";
+    }
+}
