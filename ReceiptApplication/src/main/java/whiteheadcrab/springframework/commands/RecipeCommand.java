8ac7e4d71bae3512a385ed3cf3ef28285f776108
Whiteheadcrab <lettersorstudy@gmail.com>
2020-07-17 15:31:50 +0200

    Version 1.11.2 Added RecipeCommand . Need to implement IngredientCommand and NotesCommand objects

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/commands/RecipeCommand.java
@@ -0,0 +1,29 @@
+package whiteheadcrab.springframework.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import whiteheadcrab.springframework.domain.Difficulty;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand
+{
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Difficulty difficulty;
+    private NotesCommand notes;
+    private Set<CategoryCommand> categoryCommands = new HashSet<>();
+}
