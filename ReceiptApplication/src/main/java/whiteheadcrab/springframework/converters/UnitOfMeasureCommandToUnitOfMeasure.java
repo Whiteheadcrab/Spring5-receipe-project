5a188799218d319ca55101663c9f72af28b855ec
Whiteheadcrab <lettersorstudy@gmail.com>
2020-07-21 19:55:42 +0200

    Version 1.12.1 Started implemented converters

---
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,28 @@
+package whiteheadcrab.springframework.converters;
+
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import whiteheadcrab.springframework.commands.UnitofMeasureCommand;
+import whiteheadcrab.springframework.domain.UnitOfMeasure;
+
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitofMeasureCommand, UnitOfMeasure>
+{
+    @Nullable
+    @Synchronized
+    @Override
+    public UnitOfMeasure convert(UnitofMeasureCommand source)
+    {
+        if (source == null)
+        {
+            return null;
+        }
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
+}
