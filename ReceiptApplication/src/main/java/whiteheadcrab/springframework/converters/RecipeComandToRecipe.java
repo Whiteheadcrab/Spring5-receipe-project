5e8fe848aa3771b2e7775e3a0ff29c8f98a30289
Whiteheadcrab <lettersorstudy@gmail.com>
2020-07-27 15:10:14 +0200

    Version 1.12.5 Implemented converter from RecipeCommand into Recipe .

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/converters/RecipeComandToRecipe.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/converters/RecipeComandToRecipe.java
@@ -1,6 +1,8 @@
 package whiteheadcrab.springframework.converters;
 
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import whiteheadcrab.springframework.commands.RecipeCommand;
 import whiteheadcrab.springframework.domain.Recipe;
@@ -8,5 +10,38 @@ import whiteheadcrab.springframework.domain.Recipe;
 @Component
 public class RecipeComandToRecipe implements Converter<RecipeCommand, Recipe>
 {
-  //  public RecipeCommand convert(RecipeCommand recipeCommand)
+    private final CategoryCommandToCategory categoryCommandToCategory;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
+    private final NotesCommandToNotes notesCommandToNotes;
+
+    public RecipeComandToRecipe(CategoryCommandToCategory categoryCommandToCategory ,
+                                IngredientCommandToIngredient ingredientCommandToIngredient,
+                                NotesCommandToNotes notesCommandToNotes)
+    {
+        this.categoryCommandToCategory = categoryCommandToCategory;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
+        this.notesCommandToNotes = notesCommandToNotes;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand recipeCommand)
+    {
+        if (recipeCommand == null) {
+            return null;
+        }
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(recipeCommand.getId());
+        recipe.setCookTime(recipeCommand.getCookTime());
+        recipe.setPrepTime(recipeCommand.getPrepTime());
+        recipe.setDescription(recipeCommand.getDescription());
+        recipe.setDifficulty(recipeCommand.getDifficulty());
+        recipe.setServings(recipeCommand.getServings());
+        recipe.setSource(recipeCommand.getSource());
+        recipe.setNotes(notesCommandToNotes.convert(recipeCommand.getNotes()));
+
+        return recipe;
+    }
 }
