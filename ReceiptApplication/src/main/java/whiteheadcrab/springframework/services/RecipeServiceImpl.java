ddf36dbee2f49d164c9a59e693cbfefdfe7cafff
Whiteheadcrab <lettersorstudy@gmail.com>
2020-08-25 20:13:20 +0200

    Version 1.16.2 Updated delete function

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/services/RecipeServiceImpl.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/services/RecipeServiceImpl.java
@@ -65,4 +65,9 @@ public class RecipeServiceImpl implements RecipeService
         log.debug("Saved RecipeId:" + savedRecipe.getId());
         return recipeToCommandRecipe.convert(savedRecipe);
     }
+
+    //@Override
+    public void deleteById(Long idToDelete) {
+        recipeRepositories.deleteById(idToDelete);
+    }
 }
--- ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/RecipeControllerTest.java
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/RecipeControllerTest.java
@@ -13,7 +13,7 @@ import whiteheadcrab.springframework.services.RecipeService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -94,5 +94,15 @@ public class RecipeControllerTest
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+
+    @Test
+    public void testDeleteAction() throws Exception {
+        mockMvc.perform(get("/recipe/1/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        verify(recipeService, times(1)).deleteById(anyLong());
     }
 }
