cc7e30e30881e6d01723dea0689e33012f2af464
Whiteheadcrab <lettersorstudy@gmail.com>
2020-08-30 16:09:35 +0200

    Version 1.17.1 Implemented IngredientControllerTest for starting implementing showing ingredients option using TDD technology

---
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package whiteheadcrab.springframework.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import whiteheadcrab.springframework.commands.RecipeCommand;
+import whiteheadcrab.springframework.services.RecipeService;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest
+{
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception
+    {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredients/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        //then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
