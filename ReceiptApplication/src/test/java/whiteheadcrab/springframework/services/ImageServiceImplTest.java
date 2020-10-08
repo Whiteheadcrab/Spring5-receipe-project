5c895fd4f5fcdfd1f289b842c84f4cfc1b6a9199
Whiteheadcrab <lettersorstudy@gmail.com>
2020-10-08 18:04:53 +0200

    Version 1.251. Implemented test for starting developing by TDD strategy

---
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,58 @@
+package whiteheadcrab.springframework.services;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import whiteheadcrab.springframework.domain.Recipe;
+import whiteheadcrab.springframework.repositories.RecipeRepositories;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+public class ImageServiceImplTest
+{
+
+    @Mock
+    RecipeRepositories recipeRepositories;
+
+    ImageService imageService;
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepositories);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring Framework Guru".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepositories.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+        verify(recipeRepositories, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+
+}
