0ac94123b32bfa7b21fdc59a50c9faa6415ea393
Whiteheadcrab <lettersorstudy@gmail.com>
2020-10-11 16:04:18 +0200

    Version 1.26.1.1 Small changes + additional comment

--- ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/ImageControllerTest.java
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/ImageControllerTest.java
@@ -77,7 +77,7 @@ public class ImageControllerTest
     {
         //given
         RecipeCommand recipeCommand = new RecipeCommand();
-        recipeCommand.setId(1L)
+        recipeCommand.setId(1L);
 
         String s = "fake image text";
         Byte[] bytesBoxed = new Byte[s.getBytes().length];
@@ -90,6 +90,7 @@ public class ImageControllerTest
             bytesBoxed[i++] = primByte;
         }
 
+        //require to implement function "setImage" in class ImageController
         recipeCommand.setImage(bytesBoxed);
 
         when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
