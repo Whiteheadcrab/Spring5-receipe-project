d07d44df9be0ca1876558e870c29b57e9d5b786c
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-19 17:48:51 +0100

    Version 1.28.2 Implemented new function in controller and created webpage for 404 error

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package whiteheadcrab.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import whiteheadcrab.springframework.commands.RecipeCommand;
 import whiteheadcrab.springframework.domain.Difficulty;
+import whiteheadcrab.springframework.exceptions.NotFoundException;
 import whiteheadcrab.springframework.services.RecipeService;
 
 @Slf4j
@@ -56,5 +59,19 @@ public class RecipeController
         log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound()
+    {
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
