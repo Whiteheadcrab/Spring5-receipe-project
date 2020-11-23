1986a4c5e564b5986d5b8af3cee67e4304d0dbbb
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-23 17:29:39 +0100

    Version 1.30.2 Implemented required functions for appearing issue "400 error Bad Request"

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
@@ -76,4 +76,20 @@ public class RecipeController
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception)
+    {
+
+        log.error("Handling Number Format exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
 }
