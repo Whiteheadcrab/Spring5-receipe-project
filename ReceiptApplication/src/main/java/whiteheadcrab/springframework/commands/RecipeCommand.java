d718ed054a79fdd2a4cdcf9ba27e2701b9fd4c95
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-27 21:59:05 +0100

    Version 1.31.2 Implemented validation using Spring MVC annotations

--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/commands/RecipeCommand.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/commands/RecipeCommand.java
@@ -3,8 +3,13 @@ package whiteheadcrab.springframework.commands;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 import whiteheadcrab.springframework.domain.Difficulty;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,13 +20,29 @@ import java.util.Set;
 public class RecipeCommand
 {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
--- ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
+++ ReceiptApplication/src/main/java/whiteheadcrab/springframework/controllers/RecipeController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import whiteheadcrab.springframework.commands.RecipeCommand;
@@ -11,10 +12,13 @@ import whiteheadcrab.springframework.domain.Difficulty;
 import whiteheadcrab.springframework.exceptions.NotFoundException;
 import whiteheadcrab.springframework.services.RecipeService;
 
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 public class RecipeController
 {
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService)
@@ -39,14 +43,24 @@ public class RecipeController
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand)
-    {
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @PostMapping("recipe/{id}/update")
+
+    @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model)
     {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
