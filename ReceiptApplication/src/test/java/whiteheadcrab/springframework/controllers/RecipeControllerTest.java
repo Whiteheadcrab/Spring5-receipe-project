9118a3447aa1d4aca29c0ce56ae91b989f8ec90d
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-27 21:58:25 +0100

    Version 1.31.1 Implemented and updated test for new Validation features

--- ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/RecipeControllerTest.java
+++ ReceiptApplication/src/test/java/whiteheadcrab/springframework/controllers/RecipeControllerTest.java
@@ -96,8 +96,27 @@ public class RecipeControllerTest
 
         mockMvc.perform(post("/recipe")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("description","some string")
+                        .param("direction","some directions")
         )
                 .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception
+    {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(model().attributeExists("recipe"))
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
 
